a31fae6739e2ba6d94fff26ca63d1d252d5e8d8d
Anderson Gonçalves <anndersonn.gonncalves@gmail.com>
2025-07-17 16:40:45 -0300

    feat: arredondando a nota

--- src/components/basicos/ComParametro.jsx
+++ src/components/basicos/ComParametro.jsx
@@ -1,12 +1,13 @@
 function ComParametro(props) {
     console.log(props);
     const status = props.nota >= 7 ? 'Aprovado' : 'Recuperação'
+    const notaInt = Math.ceil(props.nota);
     return (
         <div>
             <h2>{props.titulo}</h2>
             <strong>{props.aluno} </strong>
             tem nota:
-            <strong> {props.nota}</strong>
+            <strong> {notaInt}</strong>
             e foi
             <strong> {status} </strong>
         </div>
