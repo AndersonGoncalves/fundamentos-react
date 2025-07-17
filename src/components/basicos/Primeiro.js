2410f33e4f36da4e4366d601495238e48f54cb27
Anderson Gonçalves <anndersonn.gonncalves@gmail.com>
2025-07-17 10:26:21 -0300

    feat: Criando um arquivo de funcao e importando no index.js

---
+++ src/components/basicos/Primeiro.js
@@ -0,0 +1,11 @@
+export default function PrimeiroSemJSX() {
+    return 'Primeiro Componente Sem JSX';
+}
+
+export function SegundoComJSX() {
+    return <h2>Segundo Componente Com JSX</h2>;
+}
+
+export function TerceiroComJSX() {
+    return <h1>Terceiro Componente Com JSX</h1>;
+}
--- src/index.js
+++ src/index.js
@@ -1,18 +1,15 @@
-import { createRoot } from 'react-dom/client'
 import './index.css'
+import { createRoot } from 'react-dom/client'
+import PrimeiroSemJSX, { SegundoComJSX, TerceiroComJSX } from './components/basicos/Primeiro'
 
 const container = document.getElementById('root')
 const root = createRoot(container)
 
-const input = <input type="text" placeholder="Digite algo..." />
 root.render(
   <div>
-    <h1>Olá React!</h1>
-    {input}
-    <p>testando</p>
+    <PrimeiroSemJSX />
+    <br />
+    <SegundoComJSX />
+    <TerceiroComJSX />
   </div>
 )
-
-
-
-
