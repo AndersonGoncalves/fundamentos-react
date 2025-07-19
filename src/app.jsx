813b6972af7df2a19bdee1bf81ae3e9c3e65572d
Anderson Gonçalves <anndersonn.gonncalves@gmail.com>
2025-07-19 19:55:57 -0300

    feat: Componente card 2 (flexbox)

--- src/app.jsx
+++ src/app.jsx
@@ -1,3 +1,4 @@
+import './App.css';
 import PrimeiroSemJSX, { SegundoComJSX, TerceiroComJSX } from './components/basicos/Primeiro';
 import ComParametro from './components/basicos/ComParametro';
 import { FragmentoComplexo, FragmentoSimples } from './components/basicos/Fragmento';
@@ -5,44 +6,46 @@ import Aleatorio from './components/basicos/Aleatorio';
 import Card from './components/layout/Card';
 
 export default (_) =>
-    <div id="app">
+    <div className="App">
         <h1>Fundamentos React</h1>
 
-        <Card titulo="Card Primeiro componente">
-            <PrimeiroSemJSX />
-            <SegundoComJSX />
-            <TerceiroComJSX />
-        </Card>
+        <div className="Cards">
+            <Card titulo="Card Primeiro componente">
+                <PrimeiroSemJSX />
+                <SegundoComJSX />
+                <TerceiroComJSX />
+            </Card>
 
-        <Card titulo="Card Com Parâmetro">
-            <ComParametro
-                titulo="Primeiro componente"
-                aluno="Anderson Gonçalves"
-                nota={9.9} />
-            <ComParametro
-                titulo="Primeiro componente"
-                aluno="Micheline Gonçalves"
-                nota={10.0} />
-            <ComParametro
-                titulo="Primeiro componente"
-                aluno="Ana Esther Gonçalves"
-                nota={10.0} />
-            <ComParametro
-                titulo="Segundo componente"
-                aluno="Ana Esther Gonçalves"
-                nota={10.0} />
-        </Card>
+            <Card titulo="Card Com Parâmetro">
+                <ComParametro
+                    titulo="Primeiro componente"
+                    aluno="Anderson Gonçalves"
+                    nota={9.9} />
+                <ComParametro
+                    titulo="Primeiro componente"
+                    aluno="Micheline Gonçalves"
+                    nota={10.0} />
+                <ComParametro
+                    titulo="Primeiro componente"
+                    aluno="Ana Esther Gonçalves"
+                    nota={10.0} />
+                <ComParametro
+                    titulo="Segundo componente"
+                    aluno="Ana Esther Gonçalves"
+                    nota={10.0} />
+            </Card>
 
-        <Card titulo="Card de Fragmento Complexo">
-            <FragmentoComplexo tipo="COMPLEXO" />
-        </Card>
+            <Card titulo="Card de Fragmento Complexo">
+                <FragmentoComplexo tipo="COMPLEXO" />
+            </Card>
 
-        <Card titulo="Card de Fragmento Simples">
-            <FragmentoSimples tipo="SIMPLES" />
-        </Card>
+            <Card titulo="Card de Fragmento Simples">
+                <FragmentoSimples tipo="SIMPLES" />
+            </Card>
 
-        <Card titulo="Exemplo de Card">
-            <Aleatorio min={1} max={18} />
-        </Card>
+            <Card titulo="Exemplo de Card">
+                <Aleatorio min={1} max={18} />
+            </Card>
+        </div>
     </div>
 
